060f9994bc980442f9df0c6b730db422067ee510
Matheus Luca <luca_matheus@live.com>
2026-04-24 15:39:59 -0300

    Feat: Atualização e uso dos metodos get e set na classe main

--- br.com.alura.screenmatch/Principal.java
+++ br.com.alura.screenmatch/Principal.java
@@ -1,22 +1,27 @@
-public class Principal {
-    static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+import br.com.alura.screenmatch.modelos.Filme;
 
+public class Principal {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso castiga!");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(100);
 
         meuFilme.exibeFichaTecnica();
+
         System.out.println("*******************");
+
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println("Total de avaliações: " + meuFilme.totalDeAvaliacoes);
-        System.out.println("Soma das avaliações: " + meuFilme.somaDasAvaliacoes);
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println("Soma das avaliações: " + meuFilme.getSomaDasAvaliacoes());
+
         System.out.println("*********************");
+
         System.out.println("Media das avaliações: " + meuFilme.pegaMedia());
         System.out.println(meuFilme.getAnoDeLancamento());
-
-
     }
 }
